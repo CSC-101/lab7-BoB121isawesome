e9aec74fad0d1660f92a570d053c388fb19fcbd8
BoB121isawesome <jaydonkchen@gmail.com>
2024-11-11 20:26:53 -0800

    Lab 7 Submission

--- command_line.py
+++ command_line.py
@@ -1,1 +1,12 @@
+import sys, convert
+from types import NoneType
 
+sum=float(0)
+for i in range(1,len(sys.argv)):
+    if type(convert.str_to_float(sys.argv[i]))!= NoneType:
+        sum=sum+convert.str_to_float(sys.argv[i])
+
+
+if __name__ == '__main__':
+    print(sys.argv)
+    print(sum)
--- keyboard.py
+++ keyboard.py
@@ -1,1 +1,13 @@
+from convert import str_to_float
 
+def gather_numbers()->list[float]:
+    num_list=[]
+    while True:
+        line = input("Input a number: ")
+        if line == "done":
+            break
+        num_list.append(str_to_float(line))
+    return num_list
+
+if __name__ == '__main__':
+    print(gather_numbers())
